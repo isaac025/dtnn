439b5cd8d894ac40105cf15b8886e6b07f31b4ed
Isaac Lopez <isaac.lopez@upr.edu>
2023-11-05 21:37:31 -0400

    removed unnecessary funcs

--- src/Main.hs
+++ src/Main.hs
@@ -134,88 +134,6 @@ numHiddens' oN = oN go
         O _ -> 0
         _ :&~ n' -> 1 + go n'
 
-train ::
-    forall i hs o.
-    (KnownNat i, KnownNat o) =>
-    -- | learning rate
-    Double ->
-    -- | input vector
-    R i ->
-    -- | target vector
-    R o ->
-    -- | network to train
-    Network i hs o ->
-    Network i hs o
-train rate x0 target = fst . go x0
-  where
-    go ::
-        forall j js.
-        (KnownNat j) =>
-        R j ->
-        -- \^ input vector
-        Network j js o ->
-        -- \^ network to train
-        (Network j js o, R)
-    go !x (O w@(W wb wn)) =
-        let y = runLayer w x
-            -- the gradient (how much y affects the error)
-            -- logistic' is the derivative of logistic
-            o = logistic y
-            -- new bias weights and node weights
-            dEdy = logistic' y * (o - target)
-            wb' = wb - scale rate dEdy
-            wn' = wn - scale rate (dEdy `outer` x)
-            w' = W wb' wn'
-            -- bundle of derivatives for next step
-            dWs = tr wn #> dEdy
-         in (O w', dWs)
-    go !x (w@(W wb wn) :&~ n) =
-        let y = runLayer w x
-            o = logistic y
-            -- get dWs', bundle of derivatives from rest of the net
-            (n', dWs') = go o n
-            -- the gradient (how much y affects the error)
-            dEdy = logistic' y * dWs'
-            -- new bias weights and node weights
-            wb' = wb - scale rate dEdy
-            wn' = wn - scale rate (dEdy `outer` x)
-            w' = W wb' wn'
-            -- bundle of derivatives for next step
-            dWs = tr wn #> dEdy
-         in (w' :&~ n', dWs)
-
-netTest :: (MonadRandom m) => Double -> Int -> m String
-netTest rate n = do
-    inps <- replicateM n $ do
-        s <- getRandom
-        pure $ randomVector s Uniform 2 * 2 - 1
-    let inPosCirc l = l `inCircle` (fromRational 0.33, 0.33)
-        inNegCirc l = l `inCircle` (fromRational (-0.33), 0.33)
-        outs = flip map inps $ \v ->
-            if inPosCirc v || inNegCirc v
-                then fromRational 1
-                else fromRational 0
-    net0 <- randomNet 2 [16, 8] 1
-    let trainEach :: Network -> (Vector Double, Vector Double) -> Network
-        trainEach nt (i, o) = train rate i o nt
-        trained = foldl' trainEach net0 (zip inps outs)
-        outMat =
-            [ [ render (norm_2 (runNet trained (vector [x / 25 - 1, y / 10 - 1])))
-              | x <- [0 .. 50]
-              ]
-            | y <- [0.20]
-            ]
-        render r
-            | r <= 0.2 = ' '
-            | r <= 0.4 = '.'
-            | r <= 0.6 = '-'
-            | r <= 0.8 = '='
-            | otherwise = '#'
-    pure $ unlines outMat
-  where
-    inCircle :: Vector Double -> (Vector Double, Double) -> Bool
-    v `inCircle` (o, r) = norm_2 (v - o) <= r
-
 main :: IO ()
 main = do
     putStrLn "What hidden layer structure do you want?"
@@ -224,3 +142,11 @@ main = do
     case n of
         ONet (net :: Network 10 hs 3) -> do
             print net
+
+main' :: IO ()
+main' = do
+    putStrLn "What hidden layer structure do you want?"
+    hs <- readLn
+    n <- randomONet hs
+    withRandomONet' hs $ \(net :: Network 10 hs 3) -> do
+        print net
